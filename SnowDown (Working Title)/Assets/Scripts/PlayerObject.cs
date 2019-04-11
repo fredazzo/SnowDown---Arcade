4665818797bd56cd1b318157d09de282addf9a96
Fred Azzopardi <fred.azzo1997@live.co.uk>
2019-04-11 09:18:47 +0200

    Some network programming

---
+++ SnowDown (Working Title)/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour
+{
+    public GameObject playerUnitPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(playerUnitPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
