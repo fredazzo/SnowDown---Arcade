906c3353ce75237d0cca06638364cb3b54233e94
Fred Azzopardi <fred.azzo1997@live.co.uk>
2019-05-30 14:19:52 +0200

    deleted some scenes

---
+++ SnowDown (Working Title)/Assets/Scripts/Game Management/LoadingController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingController : MonoBehaviour
+{
+    public Image p1Fill;
+    public Image p2Fill;
+
+    public Animator transitionAnim;
+
+    public string nextScene;
+
+    private float timer = 0f;
+    public float maxTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        p1Fill.fillAmount = CalculateFill();
+        p2Fill.fillAmount = CalculateFill();
+
+        if (timer > maxTime)
+            StartCoroutine(OnSceneLoad(nextScene));
+    }
+
+    private float CalculateFill()
+    {
+        return timer / maxTime;
+    }
+
+    IEnumerator OnSceneLoad(string scene)
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(scene);
+    }
+
+}
